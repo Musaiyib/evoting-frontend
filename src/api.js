a377605f8959d0dc4a1f34e7274797438b99c817
Musaiyib Yakubu Usman <41808683+Musaiyib@users.noreply.github.com>
2023-10-14 09:24:32 +0000

    Changing baseUrl

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
 import axios from "axios";
 
 // export const API = axios.create({ baseURL: 'https://grad-payment-backend.herokuapp.com/api' })
-export const API = axios.create({ baseURL: 'http://localhost:4000/api' })
+export const API = axios.create({ baseURL: 'https://evoting-api-rxqm.onrender.com/api' })
